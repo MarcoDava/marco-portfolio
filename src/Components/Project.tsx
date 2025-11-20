bbfa5a8a6b7990684d405cd0e442fbd45b8c4fb0
MarcoDava <marco.a.dava@gmail.com>
2025-11-20 10:31:36 -0500

    added all icons and another project image

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -4,13 +4,13 @@ import { motion } from "motion/react"
 export default function Project(props: ProjectProps) {
   return (
     <motion.div 
-    className="project-container h-[40vh] w-[60vw]" 
+    className="project-container h-[40vh] w-[50vh]" 
     initial={{ opacity: 0, scale: 0.8 }}
     whileInView={{ opacity: 1, scale: 1 }}
     transition={{ duration: 1.2, times: [0, 1] ,scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },}}
     >
         <a href={props.projectLink}>
-            <img className="project-image" src={props.image} alt="project-pic" />
+            <img className="project-image h-[15vh] w-[45vh] rounded-[3vh]" src={props.image} alt="project-pic" />
             <h3 className="project-title">{props.projectTitle}</h3>
             <p className="project-summary">{props.projectDescription}</p>
             <span className="project-link">View Project</span>
